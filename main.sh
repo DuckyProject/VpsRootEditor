32f823a0c263a55c7a314f91093a17b36c18569f
DuckyProject <124997286+DuckyProject@users.noreply.github.com>
2023-06-30 18:12:06 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -1,24 +1,8 @@
 #!/bin/bash
-
-green(){
-    echo -e "\033[32m\033[01m$1\033[0m"
-}
-[[ $EUID -ne 0 ]] && su='sudo' 
-lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
-chattr -i /etc/passwd /etc/shadow >/dev/null 2>&1
-chattr -a /etc/passwd /etc/shadow >/dev/null 2>&1
-lsattr /etc/passwd /etc/shadow >/dev/null 2>&1
-prl=`grep PermitRootLogin /etc/ssh/sshd_config`
-pa=`grep PasswordAuthentication /etc/ssh/sshd_config`
-if [[ -n $prl && -n $pa ]]; then
-echo root:$1 | $su chpasswd root
-$su sed -i 's/^#\?PermitRootLogin.*/PermitRootLogin yes/g' /etc/ssh/sshd_config;
-$su sed -i 's/^#\?PasswordAuthentication.*/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
-$su service sshd restart
-sleep 10
+echo root:$1 | sudo chpasswd root
+sudo sed -i 's/#PermitRootLogin prohibit-password/PermitRootLogin yes/g' /etc/ssh/sshd_config;
+sudo sed -i 's/PasswordAuthentication no/PasswordAuthentication yes/g' /etc/ssh/sshd_config;
+sudo service sshd restart
 sudo rm ~/.ssh/authorized_keys
 iptables -P INPUT ACCEPT && iptables -P OUTPUT ACCEPT &&  iptables -F && iptables-save > /etc/iptables/rules.v4 && ip6tables-save > /etc/iptables/rules.v6 
 echo "成功更改当前root密码为：$1"
-else
-echo "当前vps不支持root账户或无法自定义root密码" && exit 1
-fi
